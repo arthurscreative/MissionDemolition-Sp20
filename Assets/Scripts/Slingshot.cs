48bbba52992e303104ae3dfbe3d90fe964aae10c
arthurscreative <arthur.silva@rutgers.edu>
2020-02-13 13:34:56 -0500

    Deactivate launchopint and finished the section of code

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class Slingshot : MonoBehaviour
 {
+    public GameObject launchPoint;
     void OnMouseEnter()
     {
-        print("Slingshot:OnMouseEnter()");
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive (true);
     }
 
     void OnMouseExit()
     {
-        print("Slingshot:OnMouseExit()");   
+        // print("Slingshot:OnMouseExit()");   
+        launchPoint.SetActive(false);
     }
+
+    void Awake()
+    {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive(false);
+    }
+
+
 }
